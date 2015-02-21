f6fa55e5c5d57aaf037a51a3f7691f07608377eb
Devin Flynn <dfloveone@gmail.com>
2015-02-21 17:32:34 -0500

    My Lineups layout

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "150x150>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   belongs_to :user
