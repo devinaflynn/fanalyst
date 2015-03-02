5e01abe003a7609c909d5c16f9e67c269f507c62
Paulo Abreu <paulo.abreu@clustercube.com>
2015-03-02 00:56:10 +0000

    Front end no longer requires password.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   protected
 
@@ -15,4 +16,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.referer || leaderboard_path
   end
+
 end
--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,4 +1,6 @@
 class LeaderboardController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @users = User.order(avarage_score: :desc)
   end
