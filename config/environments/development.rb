6ca34e26f891b66e336e33b8c169db32f03e7120
Paulo Abreu <paulo.abreu@clustercube.com>
2015-03-14 21:00:42 +0000

    Add https to paperclip.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,7 @@ Rails.application.configure do
   #
   config.paperclip_defaults = {
       :storage => :s3,
+      :s3_protocol => :https,
       :s3_credentials => {
           :bucket => ENV['AWS_BUCKET'],
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
