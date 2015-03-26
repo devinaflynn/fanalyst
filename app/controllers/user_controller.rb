f8afb510fe9c7ec63a07551036f234109e02d722
Paulo Abreu <paulo.abreu@outlook.com>
2015-03-26 23:04:00 +0000

    Add slider.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,6 +10,26 @@ class UserController < ApplicationController
     end
 
     # everyone can see the
-    @results = Result.joins(:team).where('teams.user_id = ?', @user.id).order(created_at: :desc)
+    results = Result.joins(:team).where('teams.user_id = ?', @user.id).order(created_at: :desc)
+
+    # TODO: improve performance
+    @result_views = []
+    results.each_slice(3) { |a| @result_views << a }
   end
 end
+
+
+class ResultView
+  def initialize(image_url, score, notes)
+    @image_url = image_url
+    @score = score
+    @notes = notes
+  end
+end
+
+
+
+
+# image_tag result.image(:medium)
+# result.score.round(1)
+# result.notes %></h5>
